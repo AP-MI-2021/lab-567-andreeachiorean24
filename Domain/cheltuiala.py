87877b2766089e1373b0e9108e0b7f1f7882ae22
Andreea Chiorean <andreea.claudia.chiorean@stud.ubbcluj.ro>
2021-10-24 18:28:03 +0300

    am mod

---
+++ Domain/cheltuiala.py
@@ -0,0 +1,48 @@
+def creeaza_cheltuiala(nr_apartament, suma, data, tip):
+    """
+    Creeaza un dictionar cu o cheltuiala
+    :param nr_apartament:numarul apartamentului, nenul
+    :param suma:suma cheltuielii
+    :param data:data cheltuielii
+    :param tip:tipul cheltuielii
+    :return:o cheltuiala
+    """
+    return{
+        'nr_ap': nr_apartament,
+        'suma': suma,
+        'data': data,
+        'tip': tip,
+    }
+def get_nr_ap(cheltuiala):
+    """
+    Getter pt nr apartamentului
+    :param cheltuiala: cheltuiala
+    :return: nr apartamentului dat ca parametru
+    """
+    return cheltuiala['nr_ap']
+
+def get_suma(cheltuiala):
+    """
+    Getter pt suma cheltuielii
+    :param cheltuiala:  cheltuiala
+    :return: suma cheltuielii data ca parametru
+    """
+    return cheltuiala['suma']
+def get_data(cheltuiala):
+    """
+    Getter pt data cheltuielii
+    :param cheltuiala: cheltuiala
+    :return: data cheltuielii data ca parametru
+    """
+    return cheltuiala['data']
+
+def get_tip(cheltuiala):
+    """
+    Getter pt tipul cheltuielii
+    :param cheltuiala: cheltuiala
+    :return: tipul cheltuielii dat ca parametru: intretinere, canal, alte cheltuieli
+    """
+    return cheltuiala['tip']
+
+def get_str(cheltuiala):
+    return  f'cheltuiala din apartamentu {get_nr_ap(cheltuiala)}, cu suma de {get_suma(cheltuiala)}, din data de {get_data(cheltuiala)}, tipul {get_tip(cheltuiala)}'
---
+++ Logic/crud.py
@@ -0,0 +1,58 @@
+from Domain.cheltuiala import creeaza_cheltuiala, get_nr_ap
+
+
+def adauga(lista, nr_apartament, suma, data, tip):
+    """
+    Adauga o cheltuiala intr-o lista.
+    :param lista: lista de cheltuieli
+    :param nr_apartament: numarul apartamentului, nenul
+    :param suma:suma cheltuielii
+    :param data:data cheltuielii
+    :param tip:tipul cheltuielii
+    :return: o noua lista cu cheltuiala noua adaugata
+    """
+    cheltuiala = creeaza_cheltuiala(nr_apartament, suma, data, tip)
+    return lista + [cheltuiala]
+def citire(lista, nr_apartament=None):
+    """
+    Citeste o cheltuiala din "baza de date"
+    :param lista: lista de cheltuieli
+    :param nr_apartament: numarul ap cheltuielii
+    :return: cheltuiala cu nr ap nr_apartament sau lista cu cheltuieli daca nr_apartament=None
+    """
+    cheltuiala_cu_nr_ap=None
+    for cheltuiala in lista:
+        if get_nr_ap(cheltuiala) == nr_apartament:
+            cheltuiala_cu_nr_ap=cheltuiala
+
+    if cheltuiala_cu_nr_ap:
+        return cheltuiala_cu_nr_ap
+    return lista
+
+def modificare(lista, new_cheltuiala):
+    """
+    Modifica o cheltuiala
+    :param lista: lista cu cheltuieli
+    :param new_cheltuiala: cheltuiala care se va modifica, nr_apartament trebuie sa fie unul existent
+    :return: o lista cu cheltuiala modificata
+    """
+    new_cheltuieli=[]
+    for cheltuiala in lista:
+        if get_nr_ap(cheltuiala) != get_nr_ap(new_cheltuiala):
+            new_cheltuieli.append(cheltuiala)
+        else:
+            new_cheltuieli.append(new_cheltuiala)
+    return new_cheltuieli
+
+def stergere(lista, nr_apartament):
+    """
+    Sterge o cheltuiala din "baza de date"
+    :param lista: lista de cheltuieli
+    :param nr_apartament: numarul ap cheltuielii
+    :return: o lista de cheltuieli fara cheltuiala cu nr ap nr_apartament
+    """
+    new_cheltuiala = []
+    for cheltuiala in lista:
+        if get_nr_ap(cheltuiala) != nr_apartament:
+            new_cheltuiala.append(cheltuiala)
+    return new_cheltuiala
